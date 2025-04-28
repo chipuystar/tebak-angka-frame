6b908685298a12884f7cc6a7d3593cf901529260
chipuystar <chipuy@gmail.com>
2025-04-28 09:57:29 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,23 @@ export default function Home() {
   return (
     <>
       <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Tebak Angka</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Tebak Angka Frame Game" />
+        <meta property="og:description" content="Tebak angka 1-5 di Warpcast!" />
+        <meta property="og:image" content="https://tebak-angka-frame.vercel.app/win.png" />
         <meta name="fc:frame" content="vNext" />
+        <meta name="fc:frame:image" content="https://tebak-angka-frame.vercel.app/win.png" />
         <meta name="fc:frame:button:1" content="Tebak 1" />
         <meta name="fc:frame:button:2" content="Tebak 2" />
         <meta name="fc:frame:button:3" content="Tebak 3" />
+        <meta name="fc:frame:button:4" content="Tebak 4" />
+        <meta name="fc:frame:button:5" content="Tebak 5" />
         <meta name="fc:frame:post_url" content="https://tebak-angka-frame.vercel.app/api/guess" />
-        <title>Tebak Angka!</title>
       </head>
       <body>
-        <h1>Tebak Angka dari 1-3!</h1>
+        <h1>Guess the Number</h1>
+        <p>Open this in Warpcast to play!</p>
       </body>
     </>
   );
